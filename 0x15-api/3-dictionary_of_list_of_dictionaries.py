0c2391623dfd3d133191729d3362e8fedf9907ec
Yadamzer <yadamzerterefe09@gmail.com>
2024-07-30 02:20:45 -0700

    Task 3

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Exports to-do list information of all employees to JSON format."""
+import json
+import requests
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user_info = requests.get(url + "users").json()
+
+    with open("todo_all_employees.json", "w") as f:
+        json.dump({
+            user.get("id"): [{
+                "task": task.get("title"),
+                "completed": task.get("completed"),
+                "username": user.get("username")
+            } for task in requests.get(url + "todos",
+                                    params={"userId": user.get("id")}).json()]
+            for user in user_info}, f)
